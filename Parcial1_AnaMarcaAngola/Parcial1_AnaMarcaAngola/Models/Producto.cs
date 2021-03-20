29d2497c87f8115cd97991de16ea7c53347315dd
Ana Marca <a2019113660@estudiantes.upsa.edu.bo>
2021-03-20 10:35:32 -0400

    Se creo la clase producto

---
+++ Parcial1_AnaMarcaAngola/Parcial1_AnaMarcaAngola/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_AnaMarcaAngola.Models
+{
+    public class Producto
+    {
+        [Key]
+        public float ProductoID { get; set; }
+        [Required]
+        public float Description { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public float LastBuy { get; set; }
+    }
+}
